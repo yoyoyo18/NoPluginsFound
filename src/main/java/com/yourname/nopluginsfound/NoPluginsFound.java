74b907726fefa8935804e7aa6e9ab2b0e8829b3c
yoyoyo18 <coolsun706@gmail.com>
2026-07-12 21:37:45 +0800

    Create NoPluginsFound.java

---
+++ src/main/java/com/yourname/nopluginsfound/NoPluginsFound.java
@@ -0,0 +1,18 @@
+package com.yourname.nopluginsfound;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class NoPluginsFound extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Registers the security listener to start blocking hacked client requests
+        getServer().getPluginManager().registerEvents(new NoPluginsFoundListener(), this);
+        getLogger().info("NoPluginsFound is now guarding your server plugins!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("NoPluginsFound has been disabled.");
+    }
+}
